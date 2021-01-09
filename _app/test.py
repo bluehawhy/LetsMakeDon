42a1632f061a85643465c8f7e319a66ee6b850a5
bluehawhy <bluehawhy@gmail.com>
2021-01-10 00:15:01 +0900

    works 21-01-09 in Desktop

--- _app/test.py
+++ _app/test.py
@@ -1,11 +1,18 @@
 import pandas as pd
+import requests
+import bs4 as bs
 
+url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download'
+df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)
+#print(type(df))
+#print(df[0])
+#for i in df:
+#    print(i)
 
-df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download', header=0)[0]
-
-for i in df:
-    print(i)
-
-
+base_url = 'https://finance.naver.com/item/sise_day.nhn?code=005930&page='
+res = requests.get(base_url)
+print(res.content)
+html = bs(res.content,'html.parser')
+table0 = html.find('table',{'class':'type2'})
 
 
